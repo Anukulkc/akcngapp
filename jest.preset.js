a5634168678379c26b9e5755fa7294cb792e7f88
Anukulkc <anukulkc@yahoo.com>
2023-08-09 20:17:00 -0400

    Add code coverage

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,14 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 75,
+      lines: 80,
+    },
+  },
+};
